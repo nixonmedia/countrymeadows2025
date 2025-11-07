bd0a472a2f412a4ce8fb20555d15992c5d4b5671
Vikisha <vikisha@esceneric.com>
2025-11-07 09:59:07 +0530

    Intro Zone HTML in progress

--- parts/layouts/stylized_heading_intro_zone.php
+++ parts/layouts/stylized_heading_intro_zone.php
@@ -1,12 +1,15 @@
-<section class="stylized-heading-intro-zone">
+<section class="stylized-heading-intro-zone image-with-embellishment">
   <div class="container">
     <div class="row">
-      <div class="col-lg-7">
-        <span>Here to help</span>
+      <div class="col-lg-7 intro-content-col">
+        <span class="stylized-heading d-block text-pink font-gloss-bloom mb-4 pb-2">Here to help</span>
         <h1 class="font-medium fw-bold mb-2 pb-1">Welcome to our retirement communities.</h1>
         <div class="wysiwyg-content font-regular">
           <p>For over 40 years, Country Meadows has provided senior living at the highest standards and helped families find the right options for them. Our nine retirement communities throughout Pennsylvania and Maryland offer quality lifestyles and care with different levels of service such as <a href="#">Independent Living</a>, <a href="#">Personal Care and Assisted Living</a>, <a href="#">Memory Support</a>, <a href="#">Restorative Services</a> and <a href="#">Skilled Nursing</a>. We hope you will add Country Meadows to the senior living communities you are considering as we strive each and every day to be one of the best retirement communities.</p>
         </div>
+      </div>
+      <div class="col-lg-5">
+        <img src="<?php echo get_stylesheet_directory_uri( );?>/images/" alt="" class="img-fluid">
       </div>
     </div>
   </div>
