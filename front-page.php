c63cfeae4ada360ce015b23ba748f644a4ea325f
devnixon <dev@nixonmedia.com>
2025-11-15 10:15:19 +0530

    updated front-page.php

--- front-page.php
+++ front-page.php
@@ -1,8 +1,17 @@
-<?php get_header(); ?>
-<div class="container">
+<?php get_header(); 
+
+/**************************************
+***** Stylized Heading Intro Zone *****
+****************************************/
+get_template_part('parts/layouts/stylized_heading_intro_zone'); ?>
+
+<div class="container-fluid">
   <div class="row">
     <div class="col-lg-12">
-      Home Page Content
+      <h2>Home Page Content</h2>
+      <?php $icon = get_field('streamline_icon');
+      // var_dump($icon);
+      echo "svg here ". $icon; ?>
     </div>
   </div>
 </div>
