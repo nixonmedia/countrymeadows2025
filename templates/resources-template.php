c4481ff95bb722940da28fda9c198f900dcac061
Saroj <saroj@esceneric.com>
2025-11-27 13:25:40 +0530

    Created resource center posts UI branch and created template file named resources-template

---
+++ templates/resources-template.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Template Name: Resources
+*/
+?>
+<?php get_header(); ?>
+<section class="py-5 resources-section">
+    <div class="container-fluid">
+        <div class="row">
+            <!-- left column  -->
+            <div class="col-lg-9">
+                <div class="row justify-content-center mb-5">
+                    <div class="col-12 text-center">
+                        <h2 class="fw-bold">Start Your Journey Here</h2>
+                        <div class="underline mx-auto"></div>
+                    </div>
+                </div>
+
+                <div class="row justify-content-evenly">
+                    <!-- Card 1 -->
+                    <div class="col-md-5 resource-card  p-4 position-relative d-flex gap-3 mb-4">
+                        <div class="pe-5">
+                            <h5 class="text-blue fw-bold text-capitalize">Family & Caregivers</h5>
+                            <p>Care for the caregiver</p>
+                        </div>
+                        <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Group-2.png" class="float-end rounded" alt="">
+                        <div class="corner-badge">
+                            <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Monitor-Heart-Beat-1-Streamline-Freehand.png" alt="">
+                        </div>
+                    </div>
+                    <!-- Card 2 -->
+                    <div class="col-md-5 resource-card  p-4 position-relative d-flex gap-3 mb-4">
+                        <div class="pe-5">
+                            <h5 class="text-blue fw-bold text-capitalize">Family & Caregivers</h5>
+                            <p>Care for the caregiver</p>
+                        </div>
+                        <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Group-2.png" class="float-end rounded" alt="">
+                        <div class="corner-badge">
+                            <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Monitor-Heart-Beat-1-Streamline-Freehand.png" alt="">
+                        </div>
+                    </div>
+                    <!-- Card 3 -->
+                    <div class="col-md-5 resource-card  p-4 position-relative d-flex gap-3 mb-4">
+                        <div class="pe-5">
+                            <h5 class="text-blue fw-bold text-capitalize">Family & Caregivers</h5>
+                            <p>Care for the caregiver</p>
+                        </div>
+                        <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Group-2.png" class="float-end rounded" alt="">
+                        <div class="corner-badge">
+                            <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Monitor-Heart-Beat-1-Streamline-Freehand.png" alt="">
+                        </div>
+                    </div>
+                    <!-- Card 4 -->
+                    <div class="col-md-5 resource-card  p-4 position-relative d-flex gap-3 mb-4">
+                        <div class="pe-5">
+                            <h5 class="text-blue fw-bold text-capitalize">Family & Caregivers</h5>
+                            <p>Care for the caregiver</p>
+                        </div>
+                        <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Group-2.png" class="float-end rounded" alt="">
+                        <div class="corner-badge">
+                            <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Monitor-Heart-Beat-1-Streamline-Freehand.png" alt="">
+                        </div>
+                    </div>
+                    <!-- Card 5 -->
+                    <div class="col-md-5 resource-card  p-4 position-relative d-flex gap-3 mb-4">
+                        <div class="pe-5">
+                            <h5 class="text-blue fw-bold text-capitalize">Family & Caregivers</h5>
+                            <p>Care for the caregiver</p>
+                        </div>
+                        <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Group-2.png" class="float-end rounded" alt="">
+                        <div class="corner-badge">
+                            <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Monitor-Heart-Beat-1-Streamline-Freehand.png" alt="">
+                        </div>
+                    </div>
+                    <!-- Card 6 -->
+                    <div class="col-md-5 resource-card  p-4 position-relative d-flex gap-3 mb-4">
+                        <div class="pe-5">
+                            <h5 class="text-blue fw-bold text-capitalize">Family & Caregivers</h5>
+                            <p>Care for the caregiver</p>
+                        </div>
+                        <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Group-2.png" class="float-end rounded" alt="">
+                        <div class="corner-badge">
+                            <img src="http://localhost/countrymeadows/wp-content/uploads/2025/11/Monitor-Heart-Beat-1-Streamline-Freehand.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- RIGHT COLUMN -->
+            <div class="col-lg-3 mt-5 mt-lg-0">
+            
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="fw-bold mb-2">Resource Categories</h3>
+                        <ul class="list-unstyled categories-box m-0 resources-category-list">
+                            <li><a href="#"><i class="fas fa-home me-2"></i>Assisted Living</a></li>
+                            <li><a href="#"><i class="fas fa-users me-2"></i>Family & Caregivers</a></li>
+                            <li><a href="#"><i class="fas fa-dollar-sign me-2"></i>Finances</a></li>
+                            <li><a href="#"><i class="fas fa-heartbeat me-2"></i>Health & Wellness</a></li>
+                            <li><a href="#"><i class="fas fa-building me-2"></i>Independent Living</a></li>
+                            <li><a href="#"><i class="fas fa-play me-2"></i>Let’s Get Started</a></li>
+                            <li><a href="#"><i class="fas fa-paw me-2"></i>Lifestyle</a></li>
+                            <li><a href="#"><i class="fas fa-brain me-2"></i>Memory Care</a></li>
+                            <li><a href="#"><i class="fas fa-calendar-alt me-2"></i>Planning</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    </div>
+</section>
+
+
+
+<?php get_footer(); ?>
