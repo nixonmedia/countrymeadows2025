6865f11819ea4b4168826e6531c76410ebce441c
Saroj <saroj@esceneric.com>
2025-11-29 13:11:26 +0530

    Enhanced styling for the search bar in the content and button cards sections.

--- parts/layouts/find_next_career.php
+++ parts/layouts/find_next_career.php
@@ -24,10 +24,11 @@
 
           <!-- Filters -->
           <div class="filters p-3">
-            <h6 class="fw-bold mb-3 heading">Filter Careers<img src="../../assets/images/filter-icon.png" alt=""></h6>
+            <h6 class="fw-bold mb-3 heading">Filter Careers<img src="<?php echo get_template_directory_uri(); ?>/assets/images/filter.svg" class="ps-2" alt="filter-icon">
+</h6>
 
-            <p class="filter-title">Company Location</p>
-            <div class="form-check py-2"><input class="form-check-input" type="checkbox" id="Allentown"><label class="form-check-label">Allentown</label></div>
+            <p class="filter-title mb-2 fw-semibold">Company Location</p>
+            <div class="form-check py-2"><input class="form-check-input d-flex align-items-center" type="checkbox" id="Allentown"><label class="form-check-label">Allentown</label></div>
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">Bethlehem</label></div>
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">Forks of Easton</label></div>
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">Frederick</label></div>
@@ -38,7 +39,7 @@
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">York-West</label></div>
 
 
-            <p class="filter-title">Job Category</p>
+            <p class="filter-title mb-2 fw-semibold">Job Category</p>
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">Dining</label></div>
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">Personal Care</label></div>
             <div class="form-check py-2"><input class="form-check-input" type="checkbox"><label class="form-check-label">Admin Nursing</label></div>
@@ -53,9 +54,9 @@
 
         <!-- <div> -->
         <div class="career-alert-box rounded mt-4">
-          <h6 class="fw-bold mb-1">Country Meadows career Alerts</h6>
-          <p class="mb-3 text-muted small">Receive career alerts in your inbox!</p>
-          <button class="btn subscribe-btn d-flex align-items-center gap-2">
+          <h6 class="fw-semibold mb-1 text-capitalize">Country Meadows career Alerts</h6>
+          <p class="mb-3">Receive career alerts in your inbox!</p>
+          <button class="btn subscribe-btn d-flex align-items-center gap-2 fw-bold text-capitalize">
             Subscribe
             <i class="fa-regular fa-envelope"></i>
           </button>
@@ -72,7 +73,7 @@
 
           <!-- career Card -->
           <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded ">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold mb-2">Server</h5>
               </a>
@@ -96,7 +97,7 @@
             </div>
           </div>
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -120,7 +121,7 @@
             </div>
           </div>
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -144,7 +145,7 @@
             </div>
           </div>
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -168,32 +169,7 @@
             </div>
           </div>
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
-              <a href="" class="text-pink">
-                <h5 class="career-title text-pink fw-bold">Server</h5>
-              </a>
-
-              <div class="d-flex align-items-center gap-2 text-muted mb-2">
-                <i class="fa-regular fa-clock text-purple"></i>
-                <h6 class="mb-0 fw-normal">Part Time</h6>
-                <i class="fa-regular fa-location-dot ms-2 text-purple"></i>
-                <h6 class="mb-0 fw-normal"> Hershey</h6>
-              </div>
-
-              <h6 class="posted-date mb-0 fw-normal text-light-gray">Posted on October 16, 2025</h6>
-              <hr>
-
-              <p class="career-description">
-                Pay starts at $14.00/hour, with the opportunity to earn more based on experience.
-                A Server plays a key role in our residents’ dining experience.
-              </p>
-
-              <a href="#" class="view-more fw-medium">View More</a>
-            </div>
-          </div>
-
-           <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -218,7 +194,7 @@
           </div>
 
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -243,7 +219,7 @@
           </div>
 
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -268,7 +244,32 @@
           </div>
 
            <div class="col-lg-4 col-md-6">
-            <div class="career-card p-4 h-100 rounded">
+            <div class="career-card  bg-white p-4 h-100 rounded">
+              <a href="" class="text-pink">
+                <h5 class="career-title text-pink fw-bold">Server</h5>
+              </a>
+
+              <div class="d-flex align-items-center gap-2 text-muted mb-2">
+                <i class="fa-regular fa-clock text-purple"></i>
+                <h6 class="mb-0 fw-normal">Part Time</h6>
+                <i class="fa-regular fa-location-dot ms-2 text-purple"></i>
+                <h6 class="mb-0 fw-normal"> Hershey</h6>
+              </div>
+
+              <h6 class="posted-date mb-0 fw-normal text-light-gray">Posted on October 16, 2025</h6>
+              <hr>
+
+              <p class="career-description">
+                Pay starts at $14.00/hour, with the opportunity to earn more based on experience.
+                A Server plays a key role in our residents’ dining experience.
+              </p>
+
+              <a href="#" class="view-more fw-medium">View More</a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6">
+            <div class="career-card  bg-white p-4 h-100 rounded">
               <a href="" class="text-pink">
                 <h5 class="career-title text-pink fw-bold">Server</h5>
               </a>
@@ -298,9 +299,9 @@
         <!-- Pagination -->
         <div class="pagination-wrapper text-center mt-5 bg-light-gray-100">
            <ul class="pagination justify-content-center gap-4  mb-0">
-              <li class="page-item active"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="" href="#" class="text-decoration-none">2</a></li>
-              <li class="page-item"><a class="" href="#" class="text-decoration-none">3</a></li>
+              <li class="page-item active text-decoration-none"><a class="page-link" href="#">1</a></li>
+              <li class="page-item text-decoration-none"><a href="#" class="text-decoration-none">2</a></li>
+              <li class="page-item text-decoration-none"><a href="#" class="text-decoration-none">3</a></li>
             </ul>
         </div>
 
