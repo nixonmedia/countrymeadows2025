42400a2faf12a04a4600779490cd0c2514d483ad
Vikisha <vikisha@esceneric.com>
2025-12-08 12:45:27 +0530

    Fixed BG Pattern Sizes on desktop

--- parts/layouts/stylized_heading_intro_zone.php
+++ parts/layouts/stylized_heading_intro_zone.php
@@ -17,6 +17,28 @@ $embellishment =  $layered_image_zone['embellishment'] ?? " ";
 $video_url = get_field('video_url');
 
 $disable_sidebar_submenu = get_field("disable_sidebar_submenu");
+$hide = get_field('hide_sidebar_navigation');
+
+global $post;
+$pageids = array();
+$nav_args = array(
+    'post_type'   => 'page',
+    'numberposts' => -1,
+    'meta_key'    => 'hide_sidebar_navigation',
+    'meta_compare'=> '=',
+    'meta_value'  => 1
+);
+$pages = get_posts($nav_args);
+
+if ($pages) {
+    foreach ($pages as $p) {
+        $pageids[] = $p->ID;
+    }
+}
+
+// Always define exclude_list
+$exclude_list = !empty($pageids) ? implode(",", $pageids) : '';
+$children = wp_list_pages('exclude=' . $exclude_list . '&depth=2&title_li=&echo=0&child_of=' . $post->ID);
 
 
 // Check if standard image should be shown
@@ -74,30 +96,32 @@ if ($embellishment == 'circles') {
 $section_class = '';
 
 if ($media_column && $media_type === 'Image' && $image_type === 'Image with Embellishment') {
-  if($disable_sidebar_submenu == false) {
+  if($disable_sidebar_submenu == false && $children) {
      $section_class .= ' layered-image-zone with-sidebar-submenu';
   } else {
     $section_class .= ' layered-image-zone';
   }
 } elseif ($media_column && $media_type === 'Image' && $image_type === 'Standard') {
   $section_class .= ' image-media-column';
-  if (!$disable_sidebar_submenu) {
+  if (!$disable_sidebar_submenu && $children) {
     $section_class .= ' image-media-column with-sidebar-submenu';
   }
 } elseif ($media_column && $media_type === 'Video') {
   $section_class .= ' video-media-column';
-  if (!$disable_sidebar_submenu) {
+  if (!$disable_sidebar_submenu && $children) {
     $section_class .= ' image-media-column with-sidebar-submenu';
   }
-} elseif ($disable_sidebar_submenu == false) {
+} elseif ($disable_sidebar_submenu == false && $children) {
   $section_class .= ' with-sidebar-submenu';
 }
+
+
 if(!$media_column) {
     $content_col_class = 'col-lg-8 intro-content-col';
     $outer_content_col_class = '';
     $media_col_class = '';
 } elseif($media_column && $media_type == 'Image' && $image_type == 'Image with Embellishment') {
-  if($disable_sidebar_submenu == true) {
+  if($disable_sidebar_submenu == true || !$children) {
     $content_col_class = 'col-lg-7 intro-content-col pb-5';
     $outer_content_col_class = '';
     $media_col_class = 'col-lg-5 col-xl-4 offset-xl-1 intro-img-col';
@@ -107,7 +131,7 @@ if(!$media_column) {
     $media_col_class = 'col-lg-6 intro-img-col';
   }
 } elseif($media_column && $media_type == 'Image' && $image_type == 'Standard' || $media_column && $media_type == 'Video' && $video_url) {
-  if($disable_sidebar_submenu == true) {
+  if($disable_sidebar_submenu == true || !$children) {
     $content_col_class = 'col-lg-7 intro-content-col';
     $outer_content_col_class = '';
     $media_col_class = 'col-lg-5 intro-media-col';
@@ -129,16 +153,17 @@ if(!$media_column) {
   <section class="stylized-heading-intro-zone <?php echo $section_class . ' ' . $bg_water_color; ?> <?php if ($background_water_color != 'None'): echo $bg_water_color_position; endif; ?>">
     <div class="container-fluid">
       <div class="row flex-column-reverse flex-lg-row">
-        <?php if($disable_sidebar_submenu == false && $media_column): ?>
+        <?php if(($disable_sidebar_submenu == false && $children) && $media_column): ?>
           <div class="<?php echo $outer_content_col_class; ?> <?php echo $background_pattern_class; ?> position-relative">
             <?php if ($stylized_heading): ?>
               <span class="stylized-heading d-block text-pink font-gloss-bloom mb-4"><?php echo $stylized_heading; ?></span>
             <?php endif; ?>
             <div class="row">
-        <?php endif; ?>
+        <?php endif;?>
+        
               <?php if ( ($stylized_heading && $disable_sidebar_submenu == true) || ($disable_sidebar_submenu == false && !$media_column) || $headline || $content): ?>
-                <div class="<?php echo $content_col_class; ?> <?php if($disable_sidebar_submenu == true): echo $background_pattern_class; endif; ?> position-relative">
-                  <?php if ( ($stylized_heading && $disable_sidebar_submenu == true) || ($disable_sidebar_submenu == false && !$media_column)): ?>
+                <div class="<?php echo $content_col_class; ?> <?php if($disable_sidebar_submenu == true || $media_column == false): echo $background_pattern_class; endif; ?> position-relative">
+                  <?php if ( ($stylized_heading && $disable_sidebar_submenu == true) || ($disable_sidebar_submenu == false && !$media_column) || !$children): ?>
                     <span class="stylized-heading d-block text-pink font-gloss-bloom mb-4"><?php echo $stylized_heading; ?></span>
                   <?php endif;
                   if ($headline): ?>
@@ -254,31 +279,6 @@ if(!$media_column) {
         <?php endif; ?>
         
         <?php if(!is_page('home')):
-            $hide = get_field('hide_sidebar_navigation');
-            global $post;
-            // Always initialize your array
-            $pageids = array();
-            // Find excluded pages by ACF
-            $nav_args = array(
-                'post_type'   => 'page',
-                'numberposts' => -1,
-                'meta_key'    => 'hide_sidebar_navigation',
-                'meta_compare'=> '=',
-                'meta_value'  => 1
-            );
-            $pages = get_posts($nav_args);
-
-            if ($pages) {
-                foreach ($pages as $page) {
-                    $pageids[] = $page->ID;
-                }
-            }
-
-            // Now $pageids is always at least an empty array
-            $exclude_list = implode(",", $pageids);
-
-            $children = wp_list_pages('exclude=' . $exclude_list . '&depth=2&title_li=&echo=0&child_of=' . $post->ID);
-
             if($post->post_parent || $children):
                 $parentTitle = get_the_title($post->post_parent);
 
