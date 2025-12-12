e47a382df41098f61ab65f57060a742d31f1eb55
Vikisha <vikisha@esceneric.com>
2025-12-12 16:32:27 +0530

    Stories CPT Posts Import & Export

---
+++ single-stories.php
@@ -0,0 +1,21 @@
+<?php
+get_header();
+
+if ( have_posts() ) : ?>
+  <section class="stories-post-content py-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <?php while ( have_posts() ) : the_post(); ?>
+            <h1><?php the_title(); ?></h1>
+            <div class="story-post-content">
+                <?php the_content(); ?>
+            </div>
+          <?php endwhile; ?>
+        </div>
+      </div>
+    </div>
+  </section>  
+<?php endif;
+get_footer();
+?>
