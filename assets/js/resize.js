11bd7c453339300daa127817cb9c8cacf5691c7c
Saroj <saroj@esceneric.com>
2026-01-19 16:20:58 +0530

    Fixed the UI of Font Resizer

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -4,36 +4,44 @@
  
     $("#small").on("click", function (e) {
       e.preventDefault();
+
+        // ACTIVE STATE
+      $("#small").addClass("active");
+      $("#large").removeClass("active");
  
-      $(".wysiwyg-content p").animate({
+      $(".wysiwyg-content p , .wysiwyg-content").animate({
         "font-size": "17px",
         "line-height": "27px"
       });
  
-      $(".wysiwyg-content p.wp-caption-text").animate({
+      $(".wysiwyg-content p.wp-caption-text, .wysiwyg-content .wp-caption-text").animate({
         "font-size": "14px",
         "line-height": "22px"
       });
  
-      $(".small").css("color", "#CEB367");
-      $(".large").css("color", "white");
+      // $(".small").css("color", "#CEB367");
+      // $(".large").css("color", "white");
     });
  
     $("#large").on("click", function (e) {
       e.preventDefault();
+
+        // ACTIVE STATE
+      $("#large").addClass("active");
+      $("#small").removeClass("active");
  
-      $(".wysiwyg-content p").animate({
+      $(".wysiwyg-content p , .wysiwyg-content").animate({
         "font-size": "22px",
         "line-height": "32px"
       });
  
-      $(".wysiwyg-content p.wp-caption-text").animate({
+      $(".wysiwyg-content p.wp-caption-text , .wysiwyg-content .wp-caption-text").animate({
         "font-size": "14px",
         "line-height": "22px"
       });
  
-      $(".small").css("color", "white");
-      $(".large").css("color", "#CEB367");
+      // $(".small").css("color", "white");
+      // $(".large").css("color", "#CEB367");
     });
  
     // Sticky resizer
--- parts/font-resize.php
+++ parts/font-resize.php
@@ -1,6 +1,12 @@
-<div id="resizer">
+<!-- <div id="resizer">
     TEXT<br>
     SIZE<br>
+    <div id="small" class="small">A</div>
+    <div id="large" class="large">A</div>
+</div> -->
+
+<div id="resizer" class="bg-yellow">
+    <div class="text-uppercase text-div fw-medium text-black">FONT SIZE</div>
     <div id="small" class="small">A</div>
     <div id="large" class="large">A</div>
 </div>
