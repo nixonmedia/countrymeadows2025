a20a88dc0c9d92209b9976eabecdad0cf969f510
Harneet <harneet@esceneric.com>
2026-02-05 16:40:14 +0530

    Fixed the search pages.

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
-<?php get_header(); ?>
+<?php get_header();
+get_template_part('parts/font-resize');  ?>
 <section class="error-404 not-found pb-5 pt-lg-3 mb-lg-5">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-10 offset-lg-1">
 					<h1>Sorry, that page was not found</h1>
-					<p class="font-regular mb-0">The page you are looking for no longer exists. Please try our navigation.</p>
+					<div class="search-content font-regular mb-0">The page you are looking for no longer exists. Please try our navigation.</p>
 				</div>
 			</div>
 		</div>
